08d9673f6d522a13450bff0b818d955102569c57
Alexia <88803801+alexiarstein@users.noreply.github.com>
2023-04-18 00:59:23 -0300

    nuevas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # radiotelefonia
 A text-to NATO or Radiotelephony script to convert any given text to its equivalent in NATO alphabet
+
+[ES] convierte una string de texto por argumento al alfabeto OTAN/NATO/RADIOTELEFONICO.
---
+++ radio.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Autora: Alexia Rivera Steinberg <lachicadesistemas@gmail.com>
+# https://github.com/alexiarstein
+# GNU GPL 3.0 (See LICENSE)
+# https://www.lachicadesistemas.com.ar
+# -------------------------------------------------------------
+
+declare -A radio=(
+    [a]="alpha"
+    [b]="bravo"
+    [c]="charlie"
+    [d]="delta"
+    [e]="echo"
+    [f]="foxtrot"
+    [g]="golf"
+    [h]="hotel"
+    [i]="india"
+    [j]="juliet"
+    [k]="kilo"
+    [l]="lima"
+    [m]="mike"
+    [n]="november"
+    [o]="oscar"
+    [p]="papa"
+    [q]="quebec"
+    [r]="romeo"
+    [s]="sierra"
+    [t]="tango"
+    [u]="uniform"
+    [v]="victor"
+    [w]="whiskey"
+    [x]="xray"
+    [y]="yankee"
+    [z]="zulu"
+)
+
+argumento="$1"
+
+for (( i=0; i<${#argumento}; i++ )); do
+    char="${argumento:$i:1}"
+    otra="${radio[$char]}"
+    if [[ -z "$otra" ]]; then
+        otra="$char"
+    fi
+    printf "%s " "$otra"
+done
+echo ""
